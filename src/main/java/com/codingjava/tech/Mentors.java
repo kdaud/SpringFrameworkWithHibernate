dc4779cb513ebbf31ac94ac5d66b0f46345e416e
kdaud <dkakumirizii@gmail.com>
2019-12-20 18:46:49 +0300

    added config file

---
+++ src/main/java/com/codingjava/tech/Mentors.java
@@ -0,0 +1,16 @@
+package com.codingjava.tech;
+
+
+public class Mentors {
+	
+	private String welcomeNote;
+	
+	public void setWelcomeNote(String welcomeNote) {
+		this.welcomeNote = welcomeNote;
+	}
+	
+	public void mentors() {
+		System.out.println(welcomeNote);
+	}
+	
+}
