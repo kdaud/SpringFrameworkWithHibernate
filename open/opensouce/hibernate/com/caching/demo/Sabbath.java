0279766a92208a0e56317778c07ce64f3dba4696
kdaud <dkakumirizii@gmail.com>
2019-12-27 23:19:03 +0300

    b

---
+++ open/opensouce/hibernate/com/caching/demo/Sabbath.java
@@ -0,0 +1,13 @@
+package com.caching.demo;
+
+import java.util.Date;
+
+public class Sabbath {
+	
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date);
+		
+	}
+	
+}
