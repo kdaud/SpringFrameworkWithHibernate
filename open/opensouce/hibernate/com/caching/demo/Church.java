be62d451fbbf8d510a7e398d13956e6ceee45161
kdaud <dkakumirizii@gmail.com>
2019-12-27 22:17:09 +0300

    files added about cache level 1

--- open/opensouce/hibernate/com/caching/demo/Church.java
+++ open/opensouce/hibernate/com/caching/demo/Church.java
@@ -14,14 +14,20 @@ public class Church {
 		Configuration con = new Configuration().configure("hibernates.cfg.xml").addAnnotatedClass(Servants.class);
 		SessionFactory sf= con.buildSessionFactory();
 		Session session =sf.openSession();
+		
 		session.beginTransaction();
-		
-		obj1 = session.get(Servants.class, 3);
-		System.out.println(obj1);
-		
 		obj1 = session.get(Servants.class, 3);
 		System.out.println(obj1);
 		session.getTransaction().commit();
+		session.close();
+		
+		Session session2 = sf.openSession();
+		session2.beginTransaction();
+		obj1 = session2.get(Servants.class, 3);
+		System.out.println(obj1);
+		session2.getTransaction().commit();
+		session2.close();
+		
 	}
 	
 }
