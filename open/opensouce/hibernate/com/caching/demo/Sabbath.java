f4d6d38f42656e90f14fb493db038c82bc6e6642
kdaud <dkakumirizii@gmail.com>
2019-12-27 23:20:18 +0300

    b

--- open/opensouce/hibernate/com/caching/demo/Sabbath.java
+++ open/opensouce/hibernate/com/caching/demo/Sabbath.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 public class Sabbath {
 	
+	static {
+		System.out.println("Dont Forget the Sabbath of the Lord");
+	}
 	public static void main(String[] args) {
 		Date date = new Date();
 		System.out.println(date);
