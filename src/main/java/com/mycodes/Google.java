c2d0ca233a84d89916ceb09f8d71206c1479898a
kdaud <dkakumirizii@gmail.com>
2019-12-30 00:30:06 +0300

    added

--- src/main/java/com/mycodes/Google.java
+++ src/main/java/com/mycodes/Google.java
@@ -6,6 +6,9 @@ public class Google implements Coder {
 	public static void main(String[] args) {
 		System.out.println("HELLO JUNIOR PROGRAMMER".toLowerCase());
 		new Google().show();
+		Mentor mentor = new Mentor();
+		mentor.setLanguage("Java");
+		System.out.println(mentor.toString());
 	}
 	
 
---
+++ src/main/java/com/mycodes/Mentor.java
@@ -0,0 +1,24 @@
+package com.mycodes;
+
+
+public class Mentor {
+	
+	private String language;
+	
+	public String getLanguage() {
+		return this.language;
+	}
+	
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Mentor [language=" + language + "]";
+	}
+	
+}
