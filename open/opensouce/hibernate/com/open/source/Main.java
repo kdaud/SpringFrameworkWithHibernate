fefa04307f5c1c7bc81ace4fb9f946037d330838
kdaud <dkakumirizii@gmail.com>
2020-01-02 17:08:39 +0300

    files

--- open/opensouce/hibernate/com/open/source/Main.java
+++ open/opensouce/hibernate/com/open/source/Main.java
@@ -12,24 +12,17 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
-		Player obj = new Player(201, "Giggs", "United", 11, "EPL");
-		Player obv = new Player(210, "Loren", "Bolton", 5, "SPL");
-		Player obk = new Player(200, "Yaya", "City", 8, "EPL");
-		Player obn = new Player(222, "Carrick", "United", 6, "EPL");
-		Player obc = new Player(208, "Luiz", "Arsenal", 5, "EPL");
-		//System.out.println(obj.notifyAll());
-		System.out.println(obj.toString());
+		Player obj = new Player();
+		
+
 		Configuration con = new Configuration().configure("myconfigurationfile.cfg.xml").addAnnotatedClass(Player.class);
 		SessionFactory sf = con.buildSessionFactory();
 		Session session = sf.openSession();
 		Transaction tx = session.beginTransaction();
-		session.save(obj);
-		session.save(obv);
-		session.save(obk);
-		session.save(obn);
-		session.save(obc);
+		obj = session.get(Player.class, 5);
 		tx.commit();
 		session.close();
+		System.out.println(obj);
 	}
 	
 }
--- open/opensouce/hibernate/com/open/source/Player.java
+++ open/opensouce/hibernate/com/open/source/Player.java
@@ -19,16 +19,50 @@ public class Player {
 	
 	private String league;
 	
-	public Player(int pId, String name, String club, int number, String league) {
-		super();
-		this.pId = pId;
-		this.name = name;
-		this.club = club;
-		this.number = number;
-		this.league = league;
-	}
+	//	public Player(int pId, String name, String club, int number, String league) {
+	//		super();
+	//		this.pId = pId;
+	//		this.name = name;
+	//		this.club = club;
+	//		this.number = number;
+	//		this.league = league;
+	//	}
 	
-
+	/**
+	 * @return the pId
+	 */
+	//	public int getpId() {
+	//		return pId;
+	//	}
+	//	
+	//	/**
+	//	 * @return the name
+	//	 */
+	//	public String getName() {
+	//		return name;
+	//	}
+	//	
+	//	/**
+	//	 * @return the club
+	//	 */
+	//	public String getClub() {
+	//		return club;
+	//	}
+	//	
+	//	/**
+	//	 * @return the number
+	//	 */
+	//	public int getNumber() {
+	//		return number;
+	//	}
+	//	
+	//	/**
+	//	 * @return the league
+	//	 */
+	//	public String getLeague() {
+	//		return league;
+	//	}
+	//
 	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
