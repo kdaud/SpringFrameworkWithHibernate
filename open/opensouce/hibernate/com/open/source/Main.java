ce446ffbaa6059054d3448da791e2799b7256e8d
kdaud <dkakumirizii@gmail.com>
2020-01-02 16:41:34 +0300

    files

--- open/opensouce/hibernate/com/open/source/Main.java
+++ open/opensouce/hibernate/com/open/source/Main.java
@@ -1,5 +1,9 @@
 package com.open.source;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
 	
@@ -11,6 +15,13 @@ public class Main {
 		Player obj = new Player(201, "Giggs", "United", 11, "EPL");
 		//System.out.println(obj.notifyAll());
 		System.out.println(obj.toString());
+		Configuration con = new Configuration().configure("myconfigurationfile.cfg.xml").addAnnotatedClass(Player.class);
+		SessionFactory sf = con.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		session.save(obj);
+		tx.commit();
+		session.close();
 	}
 	
 }
