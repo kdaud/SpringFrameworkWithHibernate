bb6c42258cb2257f96546188e516df5fca36f1f3
kdaud <dkakumirizii@gmail.com>
2020-01-05 21:22:10 +0300

    files

--- open/opensouce/hibernate/com/restapi/amlearning/Main.java
+++ open/opensouce/hibernate/com/restapi/amlearning/Main.java
@@ -9,7 +9,12 @@ public class Main {
 	
 	public static void main(String[] args) {
 		new Rest("Am learning Rest Api Technology and am eyeing at GSoC 2020".toUpperCase());
-		
+		dopost(3, 7);
+	}
+	
+	public static void dopost(int c, int h) {
+		int gg = 2 * (c + h);
+		System.out.println(gg);
 	}
 	
 }
